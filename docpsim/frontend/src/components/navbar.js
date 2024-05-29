13dae802771e76172dc53e21d2032fc30a91fa93
kacpe <263973@student.pwr.edu.pl>
2024-05-29 21:26:51 +0200

    make navbar change based on url

--- docpsim/frontend/src/components/navbar.js
+++ docpsim/frontend/src/components/navbar.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaHome } from "react-icons/fa";
 import './navbar.css'
 
-const Navbar = ({team="Default team"}) => {
-  return (
-    <div className="zespol-navbar">
-      <h1 className="zespol-text">{team}</h1>
+const GreetingText = ({team, player, point}) => {
+  if (team !== "empty") {
+    return (<h1 className="zespol-text">{team}</h1>);
+  } else if(player !== "empty") {
+    return <h1 className="zespol-text">Witaj {player}!</h1>;
+  }else if (point !== "empty") {
+    return <h1 className="zespol-text">{point}</h1>;
+  }else{
+    return <h1 className="zespol-text"></h1>;
+  }
+}
+
+const ButtonContainer = ({location}) => {
+  if (location === "/" || location === "/logowanie"|| location === "/rejestracja"|| location === "/tworzenie-gry") {
+    return (
+      <Link to="/" type="button" className="zespol-button">
+            <FaHome />
+      </Link>
+    )
+  } else {
+    return(
+      <>
       <div className='navbar-buttons-containter'>
-        <Link to="/moje-gry" type="button" className="zespol-button">
+      <Link to="/moje-gry" type="button" className="zespol-button">
             Moje Gry
         </Link>
         <Link to="/zespol" type="button" className="zespol-button">
@@ -17,9 +36,19 @@ const Navbar = ({team="Default team"}) => {
             Gra
         </Link>
       </div>
-      <Link to="/rejestracja" type="button" className="zespol-button">
-          Wyloguj
+      <Link to="/" type="button" className="zespol-button">
+            Wyloguj
       </Link>
+      </>
+    )
+  }
+}
+
+const Navbar = ({team="empty", player="empty", point="empty", location}) => {
+  return (
+    <div className="zespol-navbar">
+      <GreetingText team={team} player={player} point={point}/>
+      <ButtonContainer location={location} />
     </div>
   )
 }
--- docpsim/frontend/src/layouts/MainLayout.js
+++ docpsim/frontend/src/layouts/MainLayout.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import {Outlet} from 'react-router-dom'
+import {Outlet, useLocation} from 'react-router-dom'
 import Navbar from '../components/navbar'
 
+const NavbarLocalizer = ({location}) => {
+  if (location === "/gracz") {
+    
+    return <Navbar player={"defau"} location={location}/>
+  } else if(location === "/zespol") {
+    return <Navbar team={"defaut"} location={location}/>
+  } else if (location === "/punkt" || location === "/punkt-admin") {
+    return <Navbar point={"defaul"} location={location}/>
+  } else {
+    return <Navbar location={location}/>
+  }
+}
+
 function MainLayout() {
+  const location = useLocation();
+  console.log(location.pathname);
   return (
     <>
-        <Navbar />
+        <NavbarLocalizer location={location.pathname}/>
         <Outlet />
     </>
   )
